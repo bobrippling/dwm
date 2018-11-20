86fa836001f0fc4249ceac2ff8113812738c210e
Rob Pilling <robpilling@gmail.com>
2018-11-20 18:15:56 +0000

    Rules

--- config.h
+++ config.h
@@ -36,7 +36,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     iscentered     isfloating   monitor */
-	{ "URxvt",    NULL,       NULL,       0,            1,             0,           -1 },
+	{ "URxvt",     NULL,       NULL,       0,            1,             0,           -1 },
+	{ "Firefox-esr", NULL,       NULL,       1<<2,         1,             0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1<<2,         1,             0,           -1 },
 };
 
 /* layout(s) */
